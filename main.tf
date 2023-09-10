744d0efad3970952a17a830684afabda36e368d7
kumarran <kumar.s.ranjan@oracle.com>
2023-09-11 00:22:46 +0530

    Adding main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.5.0.0/16"
+
+  tags = var.vpc_tags
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.5.0.0/25"
+
+  tags = {
+    Name = "tuts-subnet"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = "t2.large"
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = var.ec2_name
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+ami = "ami-01c647eace872fc02"
+vpc_tags = {
+  Name = "tuts-vpc"
+  foo  = "bar"
+  baz  = 123
+}
+
+ec2_name = "tf-instance"
